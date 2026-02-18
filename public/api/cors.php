1273390aec888dc919179e8dc2d60bcf965dcacb
Yogii faujdar <mehar@Yogiis-MacBook-Pro.local>
2026-02-18 14:43:26 +0530

    Fix CORS: Update .htaccess and headers

--- public/api/cors.php
+++ public/api/cors.php
@@ -9,5 +9,5 @@ header('Content-Type: application/json; charset=UTF-8');
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
-    exit();
+    die();
 }
